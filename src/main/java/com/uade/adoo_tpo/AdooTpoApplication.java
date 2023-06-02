e0c39f8a87a734ae01697e2c8d2a733d5e8bed59
serleo <serleo96.98@gmail.com>
2023-06-02 20:25:00 -0300

    first commit

---
+++ src/main/java/com/uade/adoo_tpo/AdooTpoApplication.java
@@ -0,0 +1,13 @@
+package com.uade.adoo_tpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdooTpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdooTpoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uade/adoo_tpo/AdooTpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.adoo_tpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdooTpoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
